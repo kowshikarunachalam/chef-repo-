980d5ff7adf67a79cfa4a6fce92a01dd99047fe0
root <root@kowshik687-icloud-com3.mylabserver.com>
2017-04-04 21:23:00 +0000

    first commit

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,3 @@
+default["apache"]["sites"]["kowshik687-icloud-com4"] = {"port" =>80, "domain" => "kowshik687-icloud-com4.mylabserver.com " }
+default["apache"]["sites"]["kowshik687-icloud-com3"] = {"port" =>80, "domain" => "kowshik687-icloud-com3.mylabserver.com " }
+
